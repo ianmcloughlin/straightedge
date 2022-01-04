5db015abb278bbf146b3f90eee7ca3e6b48e759b
Ian McLoughlin <mcloughlin.ian@gmail.com>
2022-01-04 19:36:32 +0000

    UI left

--- straightedge.js
+++ straightedge.js
@@ -1,37 +1,163 @@
-(function() {
+// (function() {
 
-  // List of colours for use.
-  var colours = [
-    "rgb(117, 255,  51)", 
-    "rgb(219, 255,  51)",
-    "rgb(155, 223, 211)",
-    "rgb(255, 189,  51)",
-    "rgb(162,  63,  78)",
-    "rgb( 42,  84, 109)",
-    "rgb(255, 139, 128)",
-    "rgb(163, 222, 156)",
-    "rgb(179,  51,  50)",
-    "rgb(255, 146,  66)",
-    "rgb( 66, 133, 244)",
-    "rgb(234,  67,  53)",
-    "rgb(251, 188,   5)",
-    "rgb( 52, 168,  83)"
-  ];
+  /***************************************************************************
+   * Parameters and preliminaries for display.
+  ***************************************************************************/
+
+  // Number of of SVG units between 0 and 1.
+  const unit = 100;
 
   // Line thickness.
-  var thickness = 1;
+  const thickness = 1;
   
   // Line dashes.
-  var dashes = 14;
+  const dashes = 14;
   
   // Radius of a point.
-  var dotradius = 5;
+  const dotradius = 5;
   
   // Label font size.
-  var labelfontsize = 14;
+  const labelfontsize = 14;
+
+  // SVG viewbox.
+  let viewbox = [-400, -400, 800, 800];
+
+  // Add SVG.
+  var vis = d3.select("#plane")
+  .attr("viewBox", viewbox.join(' '))
+  .attr("preserveAspectRatio", "xMinYMin meet");
+
+
+  /***************************************************************************
+   * Functions for geometry etc.
+  ***************************************************************************/
+
+  // Slope and y-intercept of line from two points - checks for vertical lines.
+  function equation_of_line(p1, p2) {    
+    
+    // Slope and y intercept.
+    let m, c;
+    
+    // Vertical line check.
+    if (p1.x === p2.x) {
+      
+      // Slope is infinite or something like that.
+      m = Infinity;
+      // Set c to the x intercept.
+      c = p1.x;
+    
+    } else {
+      
+      // Slope: (y2 - y1) / (x2 - x1).
+      m = (p2.y - p1.y) / (p2.x - p1.x);
+      // Y intercept: y - y1 = m(x - x1) => c = y1 - m(x1).
+      c = p1.y - m * p1.x;
+    
+    }
+    
+    // Return.
+    return {m, c};
+  }
+
+  // Point of intersection of two lines.
+  function intersection(L1, L2) {
+
+    // x and y coords of points of intersection.
+    let x, y;
+
+    // Non intersection or same line check.
+    if (L1.m == L2.m) {
+      
+      // Use null to signify no such point.
+      x = null;
+      y = null;
+
+    } else {
+      
+      // x of intercept.
+      let x = (L1.c - L2.c) / (L2.m - L1.m);
+      
+      // y of intercept.
+      let y = L1.m * x + L1.c;
+      
+    }
+
+    // Return.
+    return {x, y};
+
+  }
+
+  // Extreme points in viewbox of line from two points.
+  function extreme_points(p1, p2) {
+    
+    // The four values we're gonig to calculate.
+    let x1, y1, x2, y2;
+
+    // Equation of the line.
+    let {m, c} = equation_of_line(p1, p2)
+        
+    // Check if line is vertical.
+    if (m === Infinity) {
+
+      // The y values are just the top and bottom of the viewbox.
+      y1 = viewbox[1];
+      y2 = viewbox[1] + viewbox[3];
+      
+      // The x values are the x intercept.
+      x1 = c;
+      x2 = c;
+
+    } else {
+
+      // If the line is steep then use the y's as inputs.
+      if (Math.abs(m) > 1) {
+        
+        // The y values are the top and bottom of the viewbox.
+        y1 = viewbox[1];
+        y2 = viewbox[1] + viewbox[3];
+        
+        // x = (y - c) / m
+        x1 = (y1 - c) / m;
+        x2 = (y2 - c) / m;
+      
+      } else {
+        
+        // The x values are the left and right of the viewbox.
+        x1 = viewbox[0];
+        x2 = viewbox[0] + viewbox[2];
+        
+        // y = mx + c
+        y1 = (m * x1) + c;
+        y2 = (m * x2) + c;
+    
+      }
+    }
+
+    // Return the extreme line points.
+    return {x1, x2, y1, y2};
+  }
+
+  // Centre and radius of a circle from two points in an array.
+  function centre_and_radius(p1, p2) {
   
+    // Calculate radius.
+    let a = p2.x - p1.x;
+    let b = p2.y - p1.y;
+    let r = Math.sqrt(a * a + b * b);
+
+    // Return the circle parameters for the SVG element.
+    return {cx: p1.x, cy: p1.y, r};
+
+  }
+  
+
+  
+  /***************************************************************************
+   * Initial ponts, lines, and circles.
+  ***************************************************************************/
+
   // Constructed points.
-  var points = [
+  let points = [
     {
       label: "0"
       , x: 0
@@ -53,120 +179,98 @@
       , labelx: -15
       , labely: 15
     },
+    {
+      label: "A"
+      , x: 100
+      , y: 100
+      , labelx: -15
+      , labely: 15
+    },
+    {
+      label: "B"
+      , x: 200
+      , y: -100
+      , labelx: -15
+      , labely: 15
+    },
   ];
   
-  // Lines.
-  var lines = [
+  // Constructed lines.
+  let lines = [
     ["0", "1"],
+    ["0", "A"],
+    ["1", "B"],
   ];
-  
-  // Circles.
-  var circles = [
+
+  // Constructed circles.
+  let circles = [
     ["1", "0"],
+    ["0", "1"]
   ];
-  
-  // SVG viewbox.
-  var viewbox = [-400, -200, 800, 400];
-  
-  // Add SVG.
-  var vis = d3.select("#plane")
-              .attr("viewBox", viewbox.join(' '));
-  
-  // Calculate the extreme points of the lines.
-  var extremes = [];
-  for (line of lines) {
-    // Get points by label from points array.
-    var p1 = points.filter(obj => obj.label == line[0])[0];
-    var p2 = points.filter(obj => obj.label == line[1])[0];
     
-    // Locations.
-    var x1 = p1['x'];
-    var x2 = p2['x'];
-    var y1 = p1['y'];
-    var y2 = p2['y'];
-  
-    // Check if line is vertical.
-    if ((x2 - x1) == 0) {
-      // Draw vertical line
-      console.log("IMPLEMENT VERTICAL");
-    } else {
-      // Slope of line.
-      var m = (y2 - y1) / (x2 - x1);
-      // Y intercept.
-      var c = y1 - (m * x1);
-      // Left point.
-      var x = viewbox[0];
-      var left = [x, (m * x) + c];
-      // Right point.
-      var x = viewbox[0] + viewbox[2];
-      var right = [x, (m * x) + c];
-  
-      // Append line's extreme points.
-      extremes.push([left, right]);
-    }
-  }
-  
+
+  /***************************************************************************
+   * Painting the ponts, lines, and circles.
+  ***************************************************************************/
+
+  function paint() {
+
+    // The coordinates of the lines and circles in the SVG.
+    let svg_lines, svg_circles ;
+
+    // Calculate the SVG lines from the lines.
+    svg_lines = lines.map(line => {
+      // Get the points with the labels in line.
+      let p1 = points.filter(obj => obj.label == line[0])[0];
+      let p2 = points.filter(obj => obj.label == line[1])[0];
+      // Calculate the extreme points.
+      return extreme_points(p1, p2);
+    });
+
+    // Calculate the SVG circles from the circles.  
+    svg_circles = circles.map(circle => {
+      // Get the points with the labels in line.
+      let p1 = points.filter(obj => obj.label == circle[0])[0];
+      let p2 = points.filter(obj => obj.label == circle[1])[0];
+      // Calculate the radius and centre.
+      return centre_and_radius(p1, p2);
+    });
+
   // Draw the lines.
   vis.selectAll('.line')
-  .data(extremes)
-  .enter()
-  .append("line")
-  .attr('class', 'line')
-  .attr("x1", function(d) { return d[0][0]; })
-  .attr("y1", function(d) { return d[0][1]; })
-  .attr("x2", function(d) { return d[1][0]; })
-  .attr("y2", function(d) { return d[1][1]; })
-  .attr("stroke-width", thickness)
-  .attr("stroke-dasharray", dashes); 
-  
-  // Find centres and radii of circles.
-  var centre_r = [];
-  for (circle of circles) {
-    // Get points by label from points array.
-    var p1 = points.filter(obj => obj.label == circle[0])[0];
-    var p2 = points.filter(obj => obj.label == circle[1])[0];
-  
-    // Locations.
-    var x1 = p1['x'];
-    var x2 = p2['x'];
-    var y1 = p1['y'];
-    var y2 = p2['y'];
-  
-    var a = x2 - x1;
-    var b = y2 - y1;
-  
-    // Calculate radius.
-    var r = Math.sqrt(a * a + b * b);
-  
-    // Append circle centre and radius.
-    centre_r.push([[x1, y1], r]);
-  
-  }
+     .data(svg_lines)
+     .enter()
+     .append("line")
+     .attr('class', 'line')
+     .attr("x1", function(d) { return d.x1; })
+     .attr("y1", function(d) { return d.y1; })
+     .attr("x2", function(d) { return d.x2; })
+     .attr("y2", function(d) { return d.y2; })
+     .attr("stroke-width", thickness)
+     .attr("stroke-dasharray", dashes); 
   
   // Draw the circles.
   vis.selectAll('.circle')
-  .data(centre_r)
-  .enter()
-  .append("circle")
-  .attr('class', 'circle')
-  .attr("cx", function(d) { return d[0][0]; })
-  .attr("cy", function(d) { return d[0][1]; })
-  .attr("r", function(d) { return d[1]; })
-  .attr("stroke-width", thickness)
-  .attr("stroke-dasharray", dashes);
+     .data(svg_circles)
+     .enter()
+     .append("circle")
+     .attr('class', 'circle')
+     .attr("cx", function(d) { return d.cx; })
+     .attr("cy", function(d) { return d.cy; })
+     .attr("r", function(d) { return d.r; })
+     .attr("stroke-width", thickness)
+     .attr("stroke-dasharray", dashes);
   
   
   // Draw the points.
-  var gs = vis.selectAll('.point')
+  let gs = vis.selectAll('.point')
               .data(points)
               .enter()
               .append('g')
               .attr("transform", function(d){return "translate(" + d.x + "," + d.y + ")"});
-  
+
   gs.append("circle")
     .attr('class', 'point')
-    //.attr("cx", function(d) { return d['x']; })
-    //.attr("cy", function(d) { return d['y']; })
     .attr("r", dotradius);
   
   gs.append('text')
@@ -175,5 +279,29 @@
     .attr('class', 'label')
     .attr('font-size', labelfontsize)
     .text(function(d) { return d.label; });
+
+  // Draw the buttons.
+  let circle_button = vis.selectAll('.circle_button')
+     .data([{x: viewbox[0] + viewbox[2] - 40, y: viewbox[1] + viewbox[3] - 40}])
+     .enter()
+     .append('g')
+     .attr("transform", function(d) {return `translate(${d.x},${d.y})`;});
+  circle_button.append('circle')
+               .attr('cx', 10)
+               .attr('cy', 10)
+               .attr('r', 8)
+               .style('stroke', 'rgba(142, 67, 231, 0.5)')
+               .style('fill', 'white');
+  circle_button.append('circle')
+               .attr('cx', 10)
+               .attr('cy', 10)
+               .attr('r', 2)
+               .style('stroke', 'none')
+               .style('fill', 'rgb(0, 174, 255)');
+
+
+  }
+
+  paint();
   
-  })(this);
+// })(this);
